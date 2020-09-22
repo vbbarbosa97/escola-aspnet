4ac8215f6f72f42737f893c2872214de1cdf4f6e
Vinicius Batista Barbosa <vinicius.batista.barbosa34@gmail.com>
2020-09-21 23:06:55 -0300

    Configurando banco de dados Sqlite :boom:

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -1,16 +1,17 @@
+using System.Collections.Generic;
+
 namespace escola_aspnet.Models
 {
     public class Professor
     {
         public Professor() { }
-        public Professor(int id, string nome, string disciplina)
+        public Professor(int id, string nome)
         {
             this.Id = id;
-            this.Disciplina = disciplina;
             this.Nome = nome;
         }
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Disciplina { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using escola_aspnet.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace escola_aspnet
 {
@@ -25,6 +20,7 @@ namespace escola_aspnet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(db => db.UseSqlite(Configuration.GetConnectionString("ConexaoBanco")));
             services.AddControllers();
         }
 
